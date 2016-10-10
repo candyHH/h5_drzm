516799357b9bdc92a84fc8da2023a1bc502d4b56
candyhh <17764576286@163.com>
2016-10-10 14:20:24 +0800

    不带参数

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var superagent = require('superagent');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var thisUrl = req.url;
+  var src = req.query.src;
+  if(src==undefined){
+    src='null';
+  }
   var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
   console.log('shareUrl.................'+(global.browserURL + thisUrl).split('#')[0]);
   superagent
@@ -12,6 +16,7 @@ router.get('/', function(req, res, next) {
     .end(function(err2, res2) {
       if (res2 !== undefined && res2.ok) {
         res2.body.browserUrl = global.browserURL;
+        res2.body.src=src;
         res.render('index',res2.body);
       } else {
         console.error('微信分享api错误。');
@@ -22,7 +27,9 @@ router.get('/', function(req, res, next) {
 router.get('/index', function(req, res, next) {
   var thisUrl = req.url;
   var src = req.query.src;
-  console.log(src);
+  if(src==undefined){
+    src='null';
+  }
   var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
   console.log('shareUrl.................'+(global.browserURL + thisUrl).split('#')[0]);
   superagent
