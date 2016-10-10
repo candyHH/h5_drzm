b9d03cdc1e9ab29ba00068783302862256366bcf
candyhh <17764576286@163.com>
2016-10-10 11:31:58 +0800

    图片样式

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
       loop: false,
       direction: 'vertical',
   });
+
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+  var ratio = 640/1136;
+  if(windowWidth/windowHeight>ratio){
+    $('.pic-bg').css('height','100%');
+  }else{
+    $('.pic-bg').css('width','100%');
+  }
 })
 
 $('.download-btn').on('click',function () {
